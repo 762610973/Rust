4d458a6b276819dfd95097715b81a40f22384aca
XiaoLiang <762610973@qq.com>
2022-10-14 12:54:49 +0800

    ✨: 新特性-start..end的学习以及宏的分号问题

--- rust4/src/main.rs
+++ rust4/src/main.rs
@@ -1,8 +1,10 @@
 // 学习rust的所有权
 fn main() {
     println!("学习所有权");
+    let number = 5;
     for _element in 1..5 {
-        println!("{}",_element)
+        println!("{}",_element);
+        println!("number is {}",number);
     }
-    println!("end")
+    println!("end");
 }
---
+++ rust5/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    //let condition = true;
+    println!("关于println的分号问题,有时候可以加,有时候可以不加")
+}
